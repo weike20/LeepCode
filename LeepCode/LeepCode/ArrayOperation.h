9be9add3cd3fb79f431f1000facb2927dfcec250
weike20 <1059844625@qq.com>
2017-02-20 17:53:43 +0800

    更新

--- LeepCode/LeepCode/ArrayOperation.h
+++ LeepCode/LeepCode/ArrayOperation.h
@@ -109,6 +109,121 @@ vector<int> BetterYangHuiSanJiao(int rowNumber)
 	return values;
 }
 
+//前提A、B数组都已经排好序，并且A分配的内存足够存储两个数组
+int* MergeSortedArray(int *A, int m, int *B, int n)
+{
+	int i = m + n - 1;
+	int j = m - 1;
+	int k = n - 1;
+	while (i >= 0)
+	{
+		if (j >= 0 && k >= 0)
+		{
+			if (A[j] > B[k])
+			{
+				A[i] = A[j];
+				j--;
+			}
+			else
+			{
+				A[i] = B[k];
+				k--;
+			}
+		}
+		else if (j >= 0)
+		{
+			A[i] = A[j];
+			j--;
+		}
+		else if (k >= 0)
+		{
+			A[i] = B[k];
+			k--;
+		}
+		i--;
+
+	}
+	return A;
+}
+
+
+
+void OsForeachArrayElement(string title, int* A, int length)
+{
+	cout << title;
+	for (int i = 0; i < length; ++i)
+	{
+		cout << " " << A[i];
+	}
+	cout << endl;
+}
+
+void ArrayDemo()
+{
+	cout << "数组当中移除某个元素" << endl;
+	int A[5] = { 3, 2, 2, 5, 6 };
+	OsForeachArrayElement("Raw:", A, 5);
+	int aLength = RemoveElementFromArray(A, 5, 2);
+	OsForeachArrayElement("Result:", A, aLength);
+
+	cout << "从排序数组当中移除重复的元素" << endl;
+	int B[6] = { 1, 2, 2, 3, 3, 4, };
+	OsForeachArrayElement("Raw:", B, 6);
+	int bLength = RemoveDuplicatesElementFromSortedArray(B, 6);
+	OsForeachArrayElement("Result:", B, bLength);
+
+	cout << "从排序数组当中移除指定数量的相同元素" << endl;
+	int C[9] = { 1, 2, 2, 2, 3, 3, 3, 4, 4 };
+	OsForeachArrayElement("Raw:", C, 9);
+	int cLength = RemoveNumberOfDuplicatesElementFromSortedArray(C, 9, 2);
+	OsForeachArrayElement("Result:", C, cLength);
+
+
+	cout << "加一" << endl;
+	vector<int> numbers = { 1, 2, 3, 4, 9 };
+	cout << "Raw Number: ";
+	for (auto number : numbers)
+	{
+		cout << number;
+	}
+	cout << endl;
+	vector<int> result = PlusOne(numbers);
+
+	cout << "Now Number: ";
+	for (auto number : result)
+	{
+		cout << number;
+	}
+	cout << endl;
+
+
+	cout << "杨辉三角" << endl;
+	vector<vector<int>> values = YangHuiSanJiao(6);
+	for (size_t i = 0; i < values.size(); ++i)
+	{
+
+		for (size_t j = 0; j < values[i].size(); ++j)
+		{
+			cout << " " << values[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << "索引方式的杨辉三角" << endl;
+	vector<int> rowValues = BetterYangHuiSanJiao(5);
+	for (size_t i = 0; i < rowValues.size(); ++i)
+	{
+		cout << " " << rowValues[i];
+	}
+	cout << endl;
+
+	cout << "合并排好序的两个数组" << endl;
+	int D[9] = { 1, 3, 6, 9 };
+	int E[5] = { 3, 4, 6, 8, 9 };
+	int *mergeResult = MergeSortedArray(D, 4, E, 5);
+	OsForeachArrayElement("Result: ", mergeResult, 9);
+}
+
 
 
 
--- LeepCode/LeepCode/main.cpp
+++ LeepCode/LeepCode/main.cpp
@@ -5,75 +5,7 @@
 #include "ArrayOperation.h"
 
 using namespace std;
-void OsForeachArrayElement(string title,int* A,int length)
-{
-	cout << title;
-	for (int i = 0; i < length; ++i)
-	{
-		cout << " " << A[i];
-	}
-	cout << endl;
-}
 
-void ArrayDemo()
-{
-	cout << "remove a element from array !" << endl;
-	int A[5] = {3,2,2,5,6};
-	OsForeachArrayElement("Raw:", A, 5);
-	int aLength = RemoveElementFromArray(A, 5, 2);
-	OsForeachArrayElement("Result:", A, aLength);
-
-	cout << "remove duplicates from sorted array !" << endl;
-	int B[6] = { 1, 2, 2, 3, 3, 4, };
-	OsForeachArrayElement("Raw:", B, 6);
-	int bLength = RemoveDuplicatesElementFromSortedArray(B, 6);
-	OsForeachArrayElement("Result:", B, bLength);
-
-	cout << "remove a number of duplicates from sorted array !" << endl;
-	int C[9] = { 1, 2, 2, 2, 3, 3, 3, 4, 4 };
-	OsForeachArrayElement("Raw:", C, 9);
-	int cLength = RemoveNumberOfDuplicatesElementFromSortedArray(C, 9, 2);
-	OsForeachArrayElement("Result:", C, cLength);
-
-
-	cout << "Plus One !" << endl;
-	vector<int> numbers = { 1, 2, 3, 4, 9 };
-	cout << "Raw Number: ";
-	for (auto number: numbers)
-	{
-		cout << number;
-	}
-	cout << endl;
-	 vector<int> result = PlusOne(numbers);
-
-	cout << "Now Number: ";
-	for (auto number : result)
-	{
-		cout << number;
-	}
-	cout << endl;
-
-
-	cout << "yang hui san jiao !" << endl;
-	vector<vector<int>> values = YangHuiSanJiao(6);
-	for (size_t i = 0; i < values.size(); ++i)
-	{
-		
-		for (size_t j = 0; j < values[i].size(); ++j)
-		{
-			cout << " " << values[i][j];
-		}
-		cout << endl;
-	}
-
-	cout << "better yang hui san jiao !" << endl;
-	vector<int> rowValues = BetterYangHuiSanJiao(5);
-	for (size_t i = 0; i<rowValues.size(); ++i)
-	{
-		cout <<" "<< rowValues[i];
-	}
-
-}
 
 void main()
 {
